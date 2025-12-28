857f593fca79c2f8b25ca33404920eca037b8f6a
Cleopatra <elsaerk09@gmail.com>
2025-12-28 12:28:08 +0100

    Text added

--- Bingo.py
+++ Bingo.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import numpy as np
 import random
 from PIL import Image, ImageDraw
 
@@ -16,6 +15,7 @@ if st.button("Add new text"):
     possible_text.append(new_text)
 
 line_points = [[(0, 100), (500, 100)], [(0, 200), (500, 200)], [(0,300), (500, 300)], [(0, 400), (500, 400)], [(100, 0), (100, 500)], [(200, 0), (200, 500)], [(300, 1), (333.3333333333333, 500)], [(400, 0), (400, 500)]]
+text_positions = [(50, 50), (150, 50), (250, 50), (350, 50), (450, 50), (50, 150), (150, 150), (250, 150), (350, 150), (450, 150), (50, 250), (150, 250), (250, 250), (350, 250), (450, 250), (50, 350), (150, 350), (250, 350), (350, 350), (450, 350), (50, 450), (150, 450), (250, 450), (350, 450), (450, 450)]
 
 if st.button("Generate Bingo Card"):
     if len(st.session_state.possible_text) < 25:
@@ -29,4 +29,7 @@ if st.button("Generate Bingo Card"):
         img1 = ImageDraw.Draw(img)
         for line in line_points:
             img1.line(line, fill = "black", width = 10)
+        for i in range(25):
+            img1.text(text_positions[i], bingo_card_items[i], fill = "black")
+        
         st.image(img, width = 500)
