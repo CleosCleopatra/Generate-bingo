a26466e5a67865b5b2d66a22fbc9e4a719430821
Cleopatra <elsaerk09@gmail.com>
2025-12-28 13:14:46 +0100

    Possibility to remove things

--- Bingo.py
+++ Bingo.py
@@ -36,3 +36,12 @@ if st.button("Generate Bingo Card"):
             img1.text(text_positions[i], bingo_card_items[i], fill = "black")
         
         st.image(img, width = 500)
+
+
+if st.button("More info"):
+    remove = []
+    for text in st.session_state.possible_text.copy():
+        st.write(text)
+        if st.button(f"Remove {text}", key = f"remove_{text}"):
+            st.session_state.possible_text.remove(text)
+            st.experimental_rerun()
